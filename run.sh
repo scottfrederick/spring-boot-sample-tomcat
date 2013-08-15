39e5cf6393791d24fbdfd01dc5aad2e41a00da87
Scott Frederick <scottyfred@gmail.com>
2013-08-15 10:28:32 -0500

    Initial commit.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+MEMORY_LIMIT=512M
+
+# without NR
+#java -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
+
+# with NR
+java -javaagent:./newrelic/newrelic-agent-2.21.0.jar -Dnewrelic.home=./newrelic/ -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
