5ec1458eb7eb7bf509d7d8ae707c29666df8144c
Scott Frederick <scottyfred@gmail.com>
2013-08-15 11:05:27 -0500

    Updated New Relic agent jar.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ MEMORY_LIMIT=512M
 #java -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
 
 # with NR
-java -javaagent:./newrelic/newrelic-agent-2.21.0.jar -Dnewrelic.home=./newrelic/ -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
+java -javaagent:./newrelic/newrelic-agent-2.21.2.jar -Dnewrelic.home=./newrelic/ -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
