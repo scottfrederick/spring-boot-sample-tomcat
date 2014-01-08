0c1021903a23a322ffd8e5390e0b22a861311cb2
Scott Frederick <scottyfred@gmail.com>
2014-01-08 15:25:18 -0600

    Removed New Relic setup.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-MEMORY_LIMIT=512M
-
-# without NR
-#java -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
-
-# with NR
-java -javaagent:./newrelic/newrelic-agent-2.21.3.jar -Dnewrelic.home=./newrelic/ -Xss1M -Xmx$MEMORY_LIMIT -XX:MaxPermSize=$MEMORY_LIMIT -jar build/libs/spring-boot-sample-tomcat.jar
